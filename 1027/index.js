6d99fbb1a0b3b9bee442aa4641d96fbeb03f8f30
Soohyun <csh0sns@naver.com>
2021-10-27 21:23:45 +0900

    fs JSON

--- 1027/index.js
+++ 1027/index.js
@@ -1,23 +1,29 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
+
 app.use(express.urlencoded({extended: true}));  // body-parser: 바디에 담은 내용을 어떻게 읽을 건지 express 에게 알려주
 app.use(express.json())  // body-parser
 const port = 3000;
 
-// post = > { title, description, isDeleted }
-const posts = [
-    {
-        id: 1,
-        title: "post1",
-        description: "post1 description",
-        isDeleted: false,
-    }, {
-        id: 2,
-        title: "post2",
-        description: "post2 description",
-        isDeleted: false,
-    }
-];
+// post => { title, description, isDeleted }
+// const postsArray = [
+//     {
+//         id: 1,
+//         title: "post1",
+//         description: "post1 description",
+//         isDeleted: false,
+//     }, {
+//         id: 2,
+//         title: "post2",
+//         description: "post2 description",
+//         isDeleted: false,
+//     }
+// ];
+
+const postsFilePath = "/Users/soohyun-choi/Desktop/dev/SNU_webProgramming/1027/post.json";
+const posts = JSON.parse(fs.readFileSync(postsFilePath, "utf-8"));
+console.log(posts);
 
 // 글 목록 보여주기 Read
 app.get("/posts", (req, res) => {
@@ -37,6 +43,7 @@ app.post("/posts", (req, res) => {
         id: posts.length + 1,
     }
     posts.push(post);
+    fs.writeFileSync(postsFilePath, JSON.stringify(posts));
     res.send(post);
 })
 
@@ -44,8 +51,10 @@ app.post("/posts", (req, res) => {
 app.get("/posts/:id", (req, res) => {
     const {id} = req.params;
     const post = posts.find(e => e.id === parseInt(id));
-    if (post)
+    if (post) {
+        fs.writeFileSync(postsFilePath, JSON.stringify(posts));
         res.send(post);
+    }
     else
         res.sendStatus(404);
 
@@ -56,6 +65,8 @@ app.delete("/posts/:id", (req, res) => {
     const {id} = req.params;
     const post = posts.find(e => e.id === parseInt(id));
     post.isDeleted = true;
+
+    fs.writeFileSync(postsFilePath, JSON.stringify(posts));
     res.send(post);
 })
 
@@ -66,6 +77,8 @@ app.put("/posts/:id", (req, res) => {
     const {title, description} = req.body;
     post.title = title;
     post.description = description;
+
+    fs.writeFileSync(postsFilePath, JSON.stringify(posts));
     res.send(post);
 })
 
